457646f7d7740ed3413597b893d1f1b47f66097f
Gary Teh <garyjob@gmail.com>
2018-01-21 15:35:21 -0800

    updated web server to integrate model for conducting predictions

--- service/web_server.py
+++ service/web_server.py
@@ -6,6 +6,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(dir_path + '/' + '../scraper/Scripts/'))
 import good_sell_service
 
+setattr(good_sell_service, 'model_path', dir_path + '/models/')
+
 urls = (
   '/', 'index',
   '/good_sell', 'good_sell'
@@ -23,7 +25,9 @@ class good_sell:
     params = web.input(property_id=False)
     print params
     property_id = params.property_id
-    return render.good_sell(property_id)
+    predictions = good_sell_service.good_sell_service(property_id)
+    return predictions
+    # return render.good_sell(property_id)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
